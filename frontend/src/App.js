8ba15d0b1ccbd7b3874878befc3663651a5d0dbe
Bogdan Stashchuk <bstashchuk@gmail.com>
2021-02-05 19:16:25 +0200

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -16,7 +19,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
