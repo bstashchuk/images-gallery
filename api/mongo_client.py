0f798bd522a2d8c07672d97e71405cfa6d8baee7
Bogdan Stashchuk <bstashchuk@gmail.com>
2021-04-29 13:05:19 +0300

    pymongo and mongo test

---
+++ api/mongo_client.py
@@ -0,0 +1,25 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongo")
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
+MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
+
+mongo_client = MongoClient(
+    host=MONGO_URL,
+    username=MONGO_USERNAME,
+    password=MONGO_PASSWORD,
+    port=MONGO_PORT,
+)
+
+
+def insert_test_document():
+    """Inserts sample document to the test_collection in the test db"""
+    db = mongo_client.test
+    test_collection = db.test_collection
+    res = test_collection.insert_one({"name": "Bogdan", "instructor": True})
+    print(res)
