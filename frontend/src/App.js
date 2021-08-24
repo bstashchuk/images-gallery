33af9cb87fc37c4768bcf050c02fe90475d8ed0b
Bogdan Stashchuk <bstashchuk@gmail.com>
2021-08-24 10:05:45 +0300

    get saved images in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
@@ -12,6 +12,17 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
+
+  const getSavedImages = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/images`);
+      setImages(res.data || []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => getSavedImages(), []);
 
   const handleSearchSubmit = async (e) => {
     e.preventDefault();
--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -6,7 +6,7 @@ const ImageCard = ({ image, deleteImage }) => {
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
-        <Card.Title>{image.title.toUpperCase()}</Card.Title>
+        <Card.Title>{image.title?.toUpperCase()}</Card.Title>
         <Card.Text>{image.description || image.alt_description}</Card.Text>
         <Button variant="primary" onClick={() => deleteImage(image.id)}>
           Delete
