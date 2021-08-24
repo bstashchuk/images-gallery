653a2243669dcb1918d20ccc808465b65b38a2c7
Bogdan Stashchuk <bstashchuk@gmail.com>
2021-08-24 13:38:31 +0300

    saving images in the frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,6 +41,24 @@ const App = () => {
     setImages(images.filter((image) => image.id !== id));
   };
 
+  const handleSaveImage = async (id) => {
+    const imageToBeSaved = images.find((image) => image.id === id);
+    imageToBeSaved.saved = true;
+
+    try {
+      const res = await axios.post(`${API_URL}/images`, imageToBeSaved);
+      if (res.data?.inserted_id) {
+        setImages(
+          images.map((image) =>
+            image.id === id ? { ...image, saved: true } : image
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Header title="Images Gallery" />
@@ -50,7 +68,11 @@ const App = () => {
           <Row xs={1} md={2} lg={3}>
             {images.map((image, i) => (
               <Col key={i} className="pb-3">
-                <ImageCard image={image} deleteImage={handleDeleteImage} />
+                <ImageCard
+                  image={image}
+                  deleteImage={handleDeleteImage}
+                  saveImage={handleSaveImage}
+                />
               </Col>
             ))}
           </Row>
--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
-const ImageCard = ({ image, deleteImage }) => {
+const ImageCard = ({ image, deleteImage, saveImage }) => {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image.urls.small} />
@@ -10,7 +10,12 @@ const ImageCard = ({ image, deleteImage }) => {
         <Card.Text>{image.description || image.alt_description}</Card.Text>
         <Button variant="primary" onClick={() => deleteImage(image.id)}>
           Delete
-        </Button>
+        </Button>{' '}
+        {!image.saved && (
+          <Button variant="secondary" onClick={() => saveImage(image.id)}>
+            Save
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
